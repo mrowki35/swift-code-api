8844fd0c7db70db67bf06ef18de2b7ddbdf5dfcc
Przemyslaw Orlikowski <orprzemek@gmail.com>
2025-03-23 21:15:21 +0100

    Added tests for db connection

---
+++ app/src/db/db.ts
@@ -0,0 +1,33 @@
+import { Pool, PoolClient } from "pg";
+
+
+const pool = new Pool({
+    user: 'admin',
+    host: 'localhost',
+    database: 'swift_db',
+    password: 'password',
+    port: 5432,
+  max: 10, 
+  idleTimeoutMillis: 30000, 
+  connectionTimeoutMillis: 2000, 
+});
+
+
+export const connectToDb = async (): Promise<PoolClient> => {
+  try {
+    const client = await pool.connect();
+    console.debug("Connected to PostgreSQL with connection pooling");
+    return client;
+  } catch (err) {
+    console.error(`Error connecting to PostgreSQL: ${err}`);
+    throw err;
+  }
+};
+
+
+export const closeDbPool = async () => {
+  await pool.end();
+  console.debug("PostgreSQL connection pool closed.");
+};
+
+export default pool;
--- app/src/server.ts
+++ app/src/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import router_endpoints from "./routes/ApiRoutesEndpoints";
 
-
+/*
 const app = express();
 const PORT = 3000;
 
@@ -17,3 +17,29 @@ ApiApp.use( app);
 ApiApp.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+*/
+import { connectToDb } from './db/db';
+
+export const getAllRecords = async (): Promise<any[]> => {
+  const client = await connectToDb(); 
+  try {
+    const res = await client.query('SELECT * FROM swift_data');
+    return res.rows; 
+  } catch (err) {
+    console.error("Error fetching records:", err);
+    throw err;
+  } finally {
+    client.release();
+  }
+};
+
+async function main() {
+    try {
+      const records = await getAllRecords();
+      console.log("Fetched records:", records);
+    } catch (err) {
+      console.error("Error in main:", err);
+    }
+  }
+  
+  main(); 
