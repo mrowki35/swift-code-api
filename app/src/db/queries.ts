fe69c44fad663430bb7f83f8f41b8d99183b4cf2
Przemyslaw Orlikowski <orprzemek@gmail.com>
2025-03-26 17:11:30 +0100

    Adding queries for branches

--- app/src/db/queries.ts
+++ app/src/db/queries.ts
@@ -9,3 +9,14 @@ export const swift_codes_query = `SELECT address,
                                           swift_code 
                                    FROM swift_data 
                                    WHERE swift_code = $1`;
+export const branches_swift_codes_query = `SELECT address, 
+                                   name as bankName, 
+                                   country_iso2_code as countryISO2, 
+                                   CASE 
+                                       WHEN swift_code LIKE '%XXX' THEN 1 
+                                       ELSE 0 
+                                   END AS isHeadquarter, 
+                                   swift_code 
+                            FROM swift_data 
+                            WHERE SUBSTRING(swift_code FROM 1 FOR 8) = SUBSTRING($1 FROM 1 FOR 8)`;
+
--- app/src/routers/swift_codes_router.ts
+++ app/src/routers/swift_codes_router.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response, Router } from "express";
 import { connectToDb, executeQuery } from "../db/db";
-import { swift_codes_query } from "../db/queries";
+import { branches_swift_codes_query, swift_codes_query } from "../db/queries";
+import { isEmptyIdResponsePredicate } from "../validators/Predicate";
+import { handleResponse } from "../responses/ResponseHandler";
+import {HeadquarterResponse } from "../responses/HeadquarterResponse";
+import { BranchResponse } from "../responses/BranchResponse";
 
 const swift_code_router: Router = express.Router();
 
@@ -13,15 +17,55 @@ swift_code_router.get(
       try {
         conn = await connectToDb();
         const result = await executeQuery(conn,  swift_codes_query , [swiftCode]); 
-        //const branches = await 
-     
+        
+        const swiftCodeData = result[0];
+        if (swiftCodeData?.['isheadquarter']==1)
+          {
+            const branches = await executeQuery(conn,  branches_swift_codes_query , [swiftCode]); 
+            handleResponse(
+              res,
+              req.path,
+              new HeadquarterResponse(
+                swiftCodeData?.['address'],
+                swiftCodeData?.['bank_name'],
+                swiftCodeData?.['countryiso2'],
+                swiftCodeData?.['countryname'],
+                swiftCodeData?.['isheadquarter'],
+                swiftCodeData?.['swift_code'],
+                branches
+              ),
+              isEmptyIdResponsePredicate(result[0])
+            );
+
+          }
+        else {
+
+          handleResponse(
+            res,
+            req.path,
+            new BranchResponse(
+              swiftCodeData?.['address'],
+              swiftCodeData?.['bank_name'],
+              swiftCodeData?.['countryiso2'],
+              swiftCodeData?.['countryname'],
+              swiftCodeData?.['isheadquarter'],
+              swiftCodeData?.['swift_code'],
+            ),
+            isEmptyIdResponsePredicate(result[0])
+          );
+        }
   
-        res.json(result[0]);
       } catch (error) {
         res.status(500).json({ error: "Failed to connect to DB"});
+        console.debug(error);
       } 
-    
-  }
+      finally {
+        if (conn) {
+          conn.release(); 
+          console.debug(`DB connection released back to pool`);
+        }
+      }
+    }
   );
 
   
