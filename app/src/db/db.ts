3937113fa26e1eb209e6760093463c86db731a7d
mrowki35 <115719291+mrowki35@users.noreply.github.com>
2025-03-26 23:19:24 +0100

    Adding tests for endpoint responses (#5)
    * Router name refactoring
    
    * Prettier refactor
    
    * Prettier refactor test
    
    * Adding tests for failing

--- app/src/db/db.ts
+++ app/src/db/db.ts
@@ -1,24 +1,24 @@
 import { Pool, PoolClient } from "pg";
 
 const pool = new Pool({
-    user: 'admin',
-    host: 'localhost',
-    database: 'swift_db',
-    password: 'password',
-    port: 5432,
-    max: 10, 
-    idleTimeoutMillis: 30000, 
-    connectionTimeoutMillis: 2000, 
+  user: "admin",
+  host: "localhost",
+  database: "swift_db",
+  password: "password",
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
-pool.on('connect', () => {
+pool.on("connect", () => {
   console.debug("Connected to PostgreSQL)");
 });
 
 export const connectToDb = async (): Promise<PoolClient> => {
   try {
     const client = await pool.connect();
-    return client; 
+    return client;
   } catch (err) {
     console.error(`Error connecting to PostgreSQL: ${err}`);
     throw err;
@@ -32,13 +32,16 @@ export const closeDbPool = async () => {
 
 export default pool;
 
-export const executeQuery = async (conn: PoolClient, sql: string, params: any[]): Promise<any[]> => {
+export const executeQuery = async (
+  conn: PoolClient,
+  sql: string,
+  params: any[],
+): Promise<any[]> => {
   try {
     const { rows } = await conn.query(sql, params);
     return rows;
   } catch (err) {
     console.error(`executeQuery Error: ${err}`);
     throw err;
-  } 
-  
+  }
 };
--- app/src/responses/HeadquarterResponse.ts
+++ app/src/responses/HeadquarterResponse.ts
@@ -1,12 +1,12 @@
 export class HeadquarterResponse {
-  [key: string]: any; 
-    constructor(
-      public address: string, 
-      public bankName: string, 
-      public countryISO2: string, 
-      public countryName: string, 
-      public isHeadquarter: boolean,
-      public swiftCode: string,
-      public branches: string[],
-    ) {}
-  }
+  [key: string]: any;
+  constructor(
+    public address: string,
+    public bankName: string,
+    public countryISO2: string,
+    public countryName: string,
+    public isHeadquarter: boolean,
+    public swiftCode: string,
+    public branches: string[],
+  ) {}
+}
