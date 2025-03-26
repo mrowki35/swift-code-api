2f03db2c996790ab02ac03caa2297996dfc01423
Przemyslaw Orlikowski <orprzemek@gmail.com>
2025-03-26 18:01:25 +0100

    Router name refactoring

--- app/src/routes/ApiRoutesEndpoints.ts
+++ app/src/routes/ApiRoutesEndpoints.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import swift_codes_router from "../routers/swift_codes_router";
-import banks_by_country_router from "../routers/swift_code_country_router";
+import banks_by_country_router from "../routers/banks_by_country_router";
 const router_endpoints = express.Router();
 const API_ROOT: string = "/v1";;
 
