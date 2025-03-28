a1b572a69f150075f654ae9f3e570cb28ef22872
Przemyslaw Orlikowski <orprzemek@gmail.com>
2025-03-28 21:41:14 +0100

    Getting rid of not used columns

--- db/init_db.sql
+++ db/init_db.sql
@@ -4,12 +4,10 @@ CREATE TABLE IF NOT EXISTS swift_data (
     code_type VARCHAR(6),
     name VARCHAR(255),
     address VARCHAR(255),
-    town_name VARCHAR(255),
-    country_name VARCHAR(255),
-    time_zone VARCHAR(255)
+    country_name VARCHAR(255)
 );
 
 
-COPY swift_data(country_iso2_code, swift_code, code_type, name, address, town_name, country_name, time_zone)
+COPY swift_data(country_iso2_code, swift_code, code_type, name, address, country_name)
 FROM '/docker-entrypoint-initdb.d/Interns_2025_SWIFT_CODES.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', QUOTE '"');
