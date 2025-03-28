b1a0876bd7021b9fabce457ec70f11f4d3346453
Przemyslaw Orlikowski <orprzemek@gmail.com>
2025-03-28 22:32:38 +0100

    Revert "Updatng database creation script to omit not used columns"
    This reverts commit 5e4293d84eddf0e75f9f123839320c9b53b4af53.

--- db/process_csv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-awk -F, '{print $1 "," $2 "," $3 "," $4 "," $5 "," $7}' /docker-entrypoint-initdb.d/Interns_2025_SWIFT_CODES.csv > /docker-entrypoint-initdb.d/Interns_2025_SWIFT_CODES.csv
